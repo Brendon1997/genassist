6e6091727a733be0641db97538502c82c48af5aa
Xhulio Gazidede <gazidedexh23@gmail.com>
2026-06-23 10:47:00 +0200

    feat: add message_issues table with upgrade and downgrade functions

--- backend/alembic/versions/00085_add_message_issues.py
+++ backend/alembic/versions/00085_add_message_issues.py
@@ -1,7 +1,7 @@
 """add_message_issues
 
 Revision ID: b7c8d9e0f1a2
-Revises: 753019ca4eb2
+Revises: c2bce7366f69
 Create Date: 2026-06-16 00:00:00.000000
 
 """
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'b7c8d9e0f1a2'
-down_revision: Union[str, None] = '753019ca4eb2'
+down_revision: Union[str, None] = 'c2bce7366f69'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
